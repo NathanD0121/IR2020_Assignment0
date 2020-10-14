be6a2721bc7c3cbd03615e5144dc6da966119595
Nathan Deguara <deguara4@gmail.com>
2020-10-14 11:43:59 +0100

    Add files via upload

---
+++ botController.py
@@ -0,0 +1,22 @@
+#Robot control system
+#Import
+import os
+
+#Initialise isClear
+isClear = 0
+
+#Read sensor data
+#TODO
+
+#Set value of isClear
+#TODO
+
+#Move the robot forward
+while(isClear == 0):
+    os.system(w)
+    #break
+
+#Rotate the robot
+while(isClear ==1):
+    os.system(q)
+    #break
